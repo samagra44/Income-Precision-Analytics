e3e36089daff6c7319dc55a2cde134e98a1eb82d
samagra44 <samagra44@gmail.com>
2024-01-24 19:46:41 +0530

    logger file and exception file

---
+++ main.py
@@ -0,0 +1,13 @@
+# testing logger file: 
+
+# from flask import Flask
+# from income_pred.logger import logging
+
+# app = Flask(__name__)
+# @app.route('/',methods=['GET','POST'])
+# def index():
+#     logging.info("Testing the logger file")
+#     return "Hello World"
+
+# if __name__=="__main__":
+#     app.run(debug=True)
